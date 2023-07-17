9fe0a67ce9af4c337627b9bab0b36a94e996cd88
Nezerwa <eligrand2000@gmail.com>
2023-07-17 09:58:41 +0200

    Add Navigation bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        greyish: "#3a3a3a",
+        grayishwhile: "#f1f2f2",
+      },
+    },
   },
   plugins: [],
 };
